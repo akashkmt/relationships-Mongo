79e9e9e578a48b5aa9a94a7dee74d1fe4207a175
Akash Kumawat <akashkmt963@gmail.com>
2022-07-17 20:02:14 +0530

    initial

---
+++ index.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+// Blogs = {
+//     _id : "ObjectId by Default",
+//     title: String,
+//     body: String,
+//     category_ids: [ ObjectIds()]
+//     }
+// Users = {
+//     _id : "ObjectId by Default",
+//     name: String,
+//     email: String,
+//     social_profile : {
+//         linkedIn: String,
+//         facebook: String,
+//         twiter: String,
+//         github: String,
+//         instagram: String,
+//     },
+//     addresses: [{
+//         Line1: String,
+//         City: String,
+//         State: String,
+//         PinCode: String,
+//     }],
+//     Blog_id : [ObjectId()]
+//     }
+// Category = {
+//     _id : "ObjectId by Default",
+//     name: String
+// }
+// Comments = {
+//     _id: "ObjectId by Default",
+//     Blog_id: [ObjectId()],
+//     User_id: [ObjectId()],
+//     message: String,
+//     rating : String
+// }
+// Likes = {
+//     _id: "ObjectId by Default",
+//     User_id: [ObjectId()],
+//     Blog_id: [ObjectId()],
+//     emoji: String
+// }
+
+
